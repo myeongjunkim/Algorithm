35e3953b8075abebc75f860953e0d80ebd3307a7
myeongjunkim <ilikerunning@naver.com>
2022-03-17 16:56:09 +0900

    72

--- backjoon/#72_12851_숨바꼭질2.py
+++ backjoon/#72_12851_숨바꼭질2.py
@@ -3,47 +3,30 @@ from collections import deque
 
 input = sys.stdin.readline
 
-def get_gap(a,b):
-    result = a-b
-    if result<0:
-        return -result
-    return result
-
-def get_case(num, K):
-    nearby=[]
-    if visited[num-1] == False and num != 0:
-        nearby.append(num-1)
-    if visited[num+1] == False and num < K:
-        nearby.append(num+1)
-    if visited[num*2] == False :
-        if get_gap(num, K) > get_gap(num*2, K):
-            nearby.append(num*2)
-
-    return nearby
-
 
 N, K = map(int, input().split())
 
-visited=[False]*200001
-visited[N] = True
+visited=[-1]*200001
+visited[N] = 1
 queue = deque()
-queue.append(get_case(N, K))
-cnt = 1
+queue.append(N)
 while queue:
-    nearby = queue.popleft()
-    # print(nearby)
-    new_nearby = []
-    case_cnt = 0
-    for p in nearby:
-        if p == K:
-            case_cnt +=1
-        else:
-            visited[p] = True
-            new_nearby.extend(get_case(p, K))
-    if case_cnt:
-        break
-    queue.append(new_nearby)
-    cnt +=1
+    pos = queue.popleft()
 
-print(cnt)
-print(case_cnt)
+    if pos == K:
+        print(visited[pos]+1)
+        print(visited.count(visited[pos]+1))
+        break
+
+    if visited[pos-1] == -1 and pos-1 >= 0:
+        queue.append(pos-1)
+        visited[pos-1] = visited[pos] + 1
+    
+    if visited[pos+1] == -1 and pos+1 <= 100000:
+        queue.append(pos+1)
+        visited[pos+1] = visited[pos] + 1
+
+    if visited[2*pos] == -1 and 2*pos <= 200000:
+        queue.append(pos*2)
+        visited[pos*2] = visited[pos]
+    
