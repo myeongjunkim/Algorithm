87669c8344f6b1efc4e172f4caaea8cbdff721c5
myeongjunkim <ilikerunning@naver.com>
2022-03-20 19:37:01 +0900

    #77_10830_행렬거듭.py

--- backjoon/#77_10830_행렬거듭.py
+++ backjoon/#77_10830_행렬거듭.py
@@ -14,7 +14,9 @@ def square_matrix(matrix):
     new_matrix = [[0]*N for i in range(N)]
     for i in range(N):
         for j in range(N):
-            new_matrix[i][j] += matrix[i][j] * matrix[j][i]
+            for t in range(N):
+                new_matrix[i][j] += matrix[i][t] * matrix[t][j]
+            new_matrix[i][j] %= 1000
     return new_matrix
 
 def multi_matrix(matrix1, matrix2):
@@ -23,6 +25,7 @@ def multi_matrix(matrix1, matrix2):
         for j in range(N):
             for t in range(N):
                 new_matrix[i][j] += matrix1[i][t] * matrix2[t][j]
+            new_matrix[i][j] %= 1000
     return new_matrix
 
 
@@ -31,11 +34,11 @@ def multify(matrix, cnt):
     if cnt == 1:
         return matrix
 
-    matrix_next = multify(matrix, cnt//2)
+    result = multify(matrix, cnt//2)
     if cnt %2 == 0:
-        return square_matrix(matrix_next) 
+        return square_matrix(result) 
     else:
-        return multi_matrix(square_matrix(matrix_next), matrix)
+        return multi_matrix(square_matrix(result), matrix)
 
     
     
@@ -43,6 +46,8 @@ def multify(matrix, cnt):
     
 
 new_matrix = multify(matrix, cnt)
+# new_matrix = square_matrix(matrix)
+
 
 for line in new_matrix:
     output=''
