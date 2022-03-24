e400d8c02462a33575d3f253a3c456c0c81e5b42
myeongjunkim <ilikerunning@naver.com>
2022-03-24 23:32:48 +0900

    retry fail problem

--- backjoon/fail/#24_2164_카드2.py
+++ backjoon/fail/#24_2164_카드2.py
@@ -1,19 +1,15 @@
-import queue
-
+import sys
+from collections import deque
 N = int(input(''))
 
-q = queue.Queue()
+q = deque()
+q.extend(list(range(1,N+1)))
 
-for i in range(1,N+1):
-    if i%2 == 0:
-        q.put(i)
-
-while True:
+i = 0
+while q:
+    num = q.popleft()
+    if i%2 != 0:
+        q.append(num)
     i +=1
-    top=q.get()
-    if q.empty():
-        break
-    if i%2 == 0:
-        q.put(top)
 
-print(top)
+print(num)
