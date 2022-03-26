57a47711061a9fafbdb2f457099f301f5f20b8f5
myeongjunkim <ilikerunning@naver.com>
2022-03-26 15:52:42 +0900

    \#23_1874_스택수열.py

--- backjoon/fail/#23_1874_스택수열.py
+++ backjoon/fail/#23_1874_스택수열.py
@@ -1,39 +1,31 @@
 import sys
+input = sys.stdin.readline
 
-T = int(input(''))
+T = int(input())
 
-num_list = [int(sys.stdin.readline().strip()) for i in range(T)]
+num_list = [int(input().strip()) for i in range(T)]
 
-i = 1
+i= 1
 stack = []
-result=[]
-flag=True
+result = []
+flag = True
 for num in num_list:
-    if i>num:
-        if len(stack) == 0 or stack[-1] != num:
-           flag = False
-           break
-        else:
-            pop_num = stack.pop()
-            result.append("-")
-    else:
-        stack = stack + list(range(i,num+1))
-        result = result + ['+']*(num-i+1)
-        i = num+1
-        # while num not in stack:
-        #     i +=1
-        #     stack.append(i)
-        #     result.append("+")
-        pop_num = stack.pop()
-        result.append("-")
-        
-
    
-
+    while i <= num:
+        stack.append(i)
+        result.append("+")
+        i +=1
         
+    if stack[-1] == num:
+        stack.pop()
+        result.append("-")
+    else:
+        flag = False       
+        
+
 
 if flag:
     for _ in result:
         print(_)
 else:
-    print("No")
+    print("NO")
