98b6c13d05413647ce9090a54478da5d53706f55
myeongjunkim <ilikerunning@naver.com>
2022-04-06 04:56:05 +0900

    solved problem4

--- algor_assignment/assignment_2/problem4.c
+++ algor_assignment/assignment_2/problem4.c
@@ -2,5 +2,24 @@
 
 
 int main() {
-    
+    int target = 120;
+    int numlist[] = {12, 34, 37, 45, 57, 82, 99, 120, 134};
+    int numlist_len = sizeof(numlist)/sizeof(int);
+
+    int start = 0;
+    int end = numlist_len-1;
+
+    int mid = (start+end)/2;
+    while(numlist[mid]!=target){
+        if(numlist[mid]>target){
+            end = mid;
+        } else{
+            start = mid;
+        }
+        mid = (start+end)/2;
+    }
+
+    printf("%d is placed in %d-th\n",target, mid+1);
+
+
 }
