ecb42353903c463d1a31a64ae292777582fc4cb4
myeongjunkim <ilikerunning@naver.com>
2022-04-06 04:20:18 +0900

    solved problem2 of assignment2

--- algor_assignment/assignment_2/problem1.c
+++ algor_assignment/assignment_2/problem1.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
-#include <math.h>
+
+int mypow(int n, int k){
+    int sum = 1;
+    for(int i=0; i<k; i++){
+        sum *= n;
+    }
+    return sum;
+}
 
 int main() {
     
@@ -8,7 +15,7 @@ int main() {
     int numlist[] = {10, 50, 100};
     for(int i=0; i< sizeof(numlist)/sizeof(int); i++){
         int k = 1;
-        while(pow(2,k)<=numlist[i]){
+        while(mypow(2,k)<=numlist[i]){
             k++;
         };
         printf("The largest positive integer k is %d\n", k-1);
---
+++ algor_assignment/assignment_2/problem2.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+
+int main() {
+
+    char s1[100];
+    printf("단어를 입력하세요: ");
+    scanf("%s", s1);
+
+    int length = 0;
+    for (int i= 0; s1[i] != '\0'; i++){
+        length++;
+    }
+
+    int N = length/2;
+    int flag = 1;
+    for(int i= 0; i<N; i++){
+        // printf("%c and %c\n", s1[i], s1[length-1-i]);
+        if(s1[i] != s1[length-1-i]){
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag){
+        printf("%s is palindrom\n", s1);
+    } else{
+        printf("%s is not palindrom\n", s1);
+    }
+
+    return 0;
+
+}
