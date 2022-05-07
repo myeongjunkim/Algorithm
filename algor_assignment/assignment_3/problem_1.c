cd7da7acd7a2e917153fc053c7bb005ccc38bd83
myeongjunkim <ilikerunning@naver.com>
2022-05-08 00:27:10 +0900

    assignment_3 completed

--- algor_assignment/assignment_3/problem_1.c
+++ algor_assignment/assignment_3/problem_1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 #include <stdlib.h>
 
 typedef struct Node {
@@ -8,9 +9,10 @@ typedef struct Node {
 
 typedef struct LinkedList {
     Node* head;
+    int size;
 } LinkedList;
 
-int get_node(LinkedList* L, int index){
+Node traverse_node(LinkedList* L, int index){
     
     Node* pre_node = (*L).head;
     for(int i=0; i<index; i++){
@@ -18,32 +20,116 @@ int get_node(LinkedList* L, int index){
         // printf("%d\n",pre_node->data);
     }
     Node index_node = *(pre_node->next);
-    return index_node.data;
+    return index_node;
 }
+
+void append_node(LinkedList* L, Node* node){
+    Node* pos_node = (*L).head;
+    while(pos_node->next != NULL){
+        pos_node = pos_node->next;
+    }
+    pos_node->next = node;
+    L->size++;
+}
+
+void insert_node(LinkedList* L, int index, Node* node){
+    if (L->size < index){
+        printf("index out of range\n");
+    } else{
+        Node* pre_node = (*L).head;
+        for(int i=0; i<index; i++){
+            pre_node = pre_node->next;
+        }
+        node->next = pre_node->next;
+        pre_node->next = node;
+        L->size++;
+    }
+}
+
+void remove_node(LinkedList* L, int index){
+    if (L->size < index){
+        printf("index out of range\n");
+    } else{
+        Node* pre_node = (*L).head;
+        for(int i=0; i<index; i++){
+            pre_node = pre_node->next;
+        }
+        Node* temp = pre_node->next;
+        pre_node->next = pre_node->next->next;
+        free(temp); 
+        L->size--;
+    }
+}
+
+void reverse_list(LinkedList* L){
+    Node* pos_node = L->head->next; // 0번 인덱스 노드
+    Node* temp = pos_node->next; // 바로 다음 인덱스 노드
+    pos_node->next = NULL; // 0번 인덱스 next 값 NULL;
+    Node* next_node = malloc(sizeof(Node));
+    while(temp != NULL){
+        next_node = temp; 
+        temp = temp->next;
+        next_node->next = pos_node;
+        pos_node = next_node;
+    }
+    L->head->next = pos_node;
+    free(temp);
+}    
+
+void print_list(LinkedList* L){
+    Node* pos_node = L->head;
+    while(pos_node->next != NULL){
+        pos_node = pos_node->next;
+        printf("%d ", pos_node->data);
+    }
+    printf("\n");
+}
+
 
 int main(){
 
-    // srand(time(NULL));
+    srand(time(NULL));
 
     Node* head = malloc(sizeof(Node));
+    head->next = NULL;
     
     LinkedList linked_list;
     linked_list.head = head;
 
-    Node* pre_node = linked_list.head;
     for(int i=0; i<10; i++){
         Node* new_node = malloc(sizeof(Node));
         new_node->data = i;
+        // rand()%10
         new_node->next = NULL;
 
-        pre_node->next = new_node;
-        pre_node = new_node;
+        append_node(&linked_list, new_node);
     }
 
-  
-    printf("%d\n", get_node(&linked_list,9));
 
-    // printf("%d", linked_list.head->next->next->next->data);
+    Node* new_node = malloc(sizeof(Node));
+    new_node->data = 12334;
+
+    // insert_node(&linked_list,0, new_node);
+
+    
+    
+    print_list(&linked_list);
+    // printf("traverse : %d\n", traverse_node(&linked_list,9).data);
+    // printf("%d\n", linked_list.size);
+
+    reverse_list(&linked_list);
+
+    printf("reversed\n");
+    print_list(&linked_list);
+
+
+
+
+    // remove_node(&linked_list, 4);
+    // print_list(&linked_list);
+    
+    // printf("%d\n", linked_list.size);
+
 
 
 
--- algor_assignment/assignment_3/tempCodeRunnerFile.c
+++ algor_assignment/assignment_3/tempCodeRunnerFile.c
@@ -1,1 +1,4 @@
-
+emove_node(&linked_list, 4);
+    // print_list(&linked_list);
+    
+    // printf("%d\n", linked_list.size);
