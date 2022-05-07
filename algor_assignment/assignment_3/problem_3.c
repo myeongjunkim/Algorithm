09b15f09c6d4daaf613b155ed1f5db0ce853c8da
myeongjunkim <ilikerunning@naver.com>
2022-05-08 03:19:25 +0900

    problem3 completed

--- algor_assignment/assignment_3/problem_3.c
+++ algor_assignment/assignment_3/problem_3.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 int main(){
+
+    int arr[] = {8, 3, 9, NULL, NULL, 4, 7};
+    int N = sizeof(arr)/sizeof(int);
     
+    int flag = 1;
+    for(int i=0; i<N; i++){
+        if(2*i+1<N && arr[2*i+1] != NULL){
+            if(arr[i]<arr[2*i+1]){
+                flag=0;
+                break;
+            } 
+        }
+        if(2*i+2<N && arr[2*i+2] != NULL){
+            if(arr[i]>arr[2*i+2]){
+                flag=0;
+                break;
+            }
+        }
+    }
+    if(flag) printf("\nbinary search tree O\n");
+    else printf("\nbinary search tree X\n");
+
 
 }
---
+++ algor_assignment/assignment_3/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+ // for(int i=0; i<N; i++){
+    //     Node* node =  malloc(sizeof(Node));
+        
+    // }
