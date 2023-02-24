c536a986505c01d90e5e193dfd92e3aebacbaf84
MMyeong <ilikerunning@naver.com>
2023-02-24 23:03:09 +0900

    #0 parts

--- backjoon/#0_parts.py
+++ backjoon/#0_parts.py
@@ -1,7 +1,8 @@
 import sys
 num=sys.stdin.readlines()
 
-import sys
+sys.setrecursionlimit(10**6)
+
 T = int(input(''))
 
 num_list = [int(sys.stdin.readline().strip()) for i in range(T)]
@@ -21,14 +22,28 @@ queue.extendleft([1,2,3])
 deque.remove(1)
 
 
-from queue import PriorityQueue
-Q = PriorityQueue()
-# 원소 삽입
-Q.put(데이터)
-Q.put((우선순위, 데이터))
-# 원소 삭제 - 우선순위 높은 순서대로 삭제
-Q.get() 
-Q.get()[1]
+# from queue import PriorityQueue
+# Q = PriorityQueue()
+# # 원소 삽입
+# Q.put(데이터)
+# Q.put((우선순위, 데이터))
+# # 원소 삭제 - 우선순위 높은 순서대로 삭제
+# Q.get() 
+# Q.get()[1]
+
+
+# 최소힙. 오름차순 자동 정렬함
+from heapq import heappush
+from heapq import heappop
+from heapq import heapify
+
+heap = []
+heappush(heap, 4)
+heappop(heap)
+
+num_list = [1,2,3,4]
+heapify(num_list)
+
 
 
 # 람다 정렬
@@ -50,5 +65,7 @@ print(list(product([1,2,3,4], repeat=2)))
 print(list(permutations([1,2,3,4], 2)))
 print(list(combinations([1,2,3,4], 2)))
 print(list(combinations_with_replacement([1,2,3,4], 2)))
+
+
 
 
