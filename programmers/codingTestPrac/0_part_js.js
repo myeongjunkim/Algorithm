386062684960d71735c940104426494f716877e6
MMyeong <ilikerunning@naver.com>
2024-01-08 18:05:19 +0900

    매뉴리뉴얼

--- programmers/codingTestPrac/0_part_js.js
+++ programmers/codingTestPrac/0_part_js.js
@@ -1,24 +1,6 @@
 
 const readline = require("readline");
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-// let input = []
-// rl.on("line", (line) => {
-//     input.push(line.split(' ').map(el => parseInt(el)));
-// }).on('close', () => {
-//     console.log(input)
-//     process.exit();
-// });
-
-
-
-
-
-
 
 
 
