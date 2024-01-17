97a08bbc574334ef43a2cd1defc8c060b662876c
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-01-17 21:28:37 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.py
@@ -9,46 +9,49 @@ input = sys.stdin.readline
   - 벽이 있다.
   
 구현:
-  - N 이 작다. dp? 그렇기에는 벽이 존재
-  - 그래프 이론
-  - 파이프 끝 요소 nearby 찾기
-  - 
+  - dp 
+  - 각 포인트별 (가로, 세로, 대각선) 을 통해 도달할 수 있는 경우 기록
+  - 상단 혹은 좌측이 1이면 대각선으로 도달 x
+  - 위에서부터 왼-위 껍질로 dp 채우기
+    - 이전 위치에서 방향이 맞는 것만 더하기
   
 """
 
 def solution(house):
   
-  pipe =  (0,1,0)
-  if house[0][2] == 1 or house[-1][-1] == 1:
-    return
-  dfs(pipe, house)
-  
-
-def dfs(pipe, house):
-  global count
   N = len(house)
-  r, c, dir = pipe
-  if r == N-1 and c == N-1:
-    count += 1
-    return
+  dp = [ [ [0]*3 for _ in range(N) ] for _ in range(N) ]
+  for i in range(1,N):
+    if house[0][i]:
+      break
+    dp[0][i][0] = 1
     
-  if ( r+1 < N 
-    and c+1 < N 
-    and not house[r+1][c+1] and not house[r][c+1] and not house[r+1][c]
-    ):
-    dfs((r+1, c+1, 2), house)
-  if r+1 < N and house[r+1][c] == 0 and dir != 0:
-    dfs((r+1, c, 1), house)
-  if c+1 < N and house[r][c+1] == 0 and dir != 1:
-    dfs((r, c+1, 0), house)
+
+  for i in range(1, N):
+    for c in range(i, N):
+      if house[i][c] == 1:
+        continue
+      dp[i][c][0] = dp[i][c-1][0] + dp[i][c-1][2] # 가로 + 대각선
+      dp[i][c][1] = dp[i-1][c][1] + dp[i-1][c][2] # 세로 + 대각선
+      dp[i][c][2] = sum(dp[i-1][c-1]) if not house[i-1][c] and not house[i][c-1] else 0
+
+    for r in range(i+1, N):
+      if house[r][i] == 1:
+        continue
+      dp[r][i][0] = dp[r][i-1][0] + dp[r][i-1][2] # 가로 + 대각선
+      dp[r][i][1] = dp[r-1][i][1] + dp[r-1][i][2] # 세로 + 대각선
+      dp[r][i][2] = sum(dp[r-1][i-1]) if not house[r-1][i] and not house[r][i-1] else 0
+
+  # for line in dp:
+  #   print(line)
+
+  return sum(dp[N-1][N-1])
     
+  
 
 # main
 N = int(input())
 house = [ list(map(int, input().split())) for _ in range(N) ] 
-
-count = 0
-solution(house)
-print(count)
+print(solution(house))
 
       
