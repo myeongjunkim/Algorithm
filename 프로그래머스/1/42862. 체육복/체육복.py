dea583a1fe2df51a150c5a2eecbb126ff3e5cd69
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-02-25 01:28:28 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,28 @@
+def solution(n, lost, reserve):
+    
+    lost, reserve = sorted(lost), sorted(reserve)
+    visited = [False] * len(reserve)
+    
+    new_lost = []
+    for s in lost:
+        possible = False
+        for i in range(len(reserve)):
+            if s == reserve[i]:
+                visited[i] = True
+                possible = True
+        if not possible:
+            new_lost.append(s)
+                
+    
+    for s in new_lost:
+        possible = False
+        for i in range(len(reserve)):
+            if not visited[i] and reserve[i] in [s-1,s+1]:
+                visited[i] = True
+                possible = True
+                break
+        if not possible:
+            n-=1
+    
+                
+    return n
