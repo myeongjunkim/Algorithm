88721c27a396c4bcb49104cf5bd9cb40ddd4e1e6
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-02-29 02:45:53 +0900

    [level 3] Title: 여행경로, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,17 +1,3 @@
-"""
-조건:
-    - 방문하는 공항 경로를 배열에 담아 return 
-    - 주어진 공항 수는 3개 이상 10,000개 이하
-    - 가능한 경로가 2개 이상일 경우 사전순 우선 리턴
-    - 항상 "ICN" 공항에서 출발합니다.
-구현:
-    - 도시 리스트 구하고 사전순 정렬
-    - depth 가 도시 숫자면 리턴
-    - visited, _map
-예외:
-    - 모든 노드를 다 순회하는 것이 목적이 아니다. 즉 visited 관리를 하는 백트래킹 문제이다.
-"""
-
 from collections import defaultdict
 
 def solution(tickets):
