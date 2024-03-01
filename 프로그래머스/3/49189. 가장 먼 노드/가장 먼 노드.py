e4897a71e35188eeb4d8ecb965c87864fd5af233
mmyeong <82504981+myeongjunkim@users.noreply.github.com>
2024-03-01 23:55:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 77.44 ms, Memory: 27.3 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -10,6 +10,9 @@
 """
 
 from collections import deque
+from heapq import heappush, heappop
+
+import sys
 
 def solution(n, edge):
     def get_map(n, edge):
@@ -18,20 +21,56 @@ def solution(n, edge):
             _map[a].append(b)
             _map[b].append(a)
         return _map
+
+    def get_weight_map(n, edge):
+        _map = [ [] for i in range(n+1) ]
+        for a, b in edge:
+            _map[a].append((1, b))
+            _map[b].append((1, a))
+        return _map
+            
+    def bfs(_map, start):
+        visited = [0]*(n+1)
+        start_n = 1
+        q = deque([start_n])
+        while q:
+            pos_n = q.popleft()
+            for new_n in _map[pos_n]:
+                if new_n == start_n:
+                    continue
+                if visited[new_n]:
+                    continue
+                visited[new_n] = visited[pos_n]+1
+                q.append(new_n)
+        return visited
+    
+    def dijkstra(_map, start):
+        
+        dist = [sys.maxsize]*(n+1)
+        dist[0] = 0
+        dist[start] = 0
+        heap = [ (0,start) ]
+        
+        while heap:
+            pos_dist, pos_n = heappop(heap)
+            
+            if pos_dist > dist[pos_n]:
+                continue
+            for new_w, new_n in _map[pos_n]:
+                if dist[new_n] > dist[pos_n] + new_w:
+                    dist[new_n] = dist[pos_n] + new_w
+                    heappush(heap, (dist[new_n], new_n))
+        return dist
+        
+        
             
     
-    _map = get_map(n, edge)
-    visited = [0]*(n+1)
-    start_n = 1
-    q = deque([start_n])
-    while q:
-        pos_n = q.popleft()
-        for new_n in _map[pos_n]:
-            if new_n == start_n:
-                continue
-            if visited[new_n]:
-                continue
-            visited[new_n] = visited[pos_n]+1
-            q.append(new_n)
     
-    return visited.count(max(visited))
+    # _map = get_map(n, edge)
+    # dist = bfs(_map, 1)
+    
+    weight_map = get_weight_map(n, edge)
+    dist = dijkstra(weight_map, 1)
+    print(dist)
+    
+    return dist.count(max(dist))
